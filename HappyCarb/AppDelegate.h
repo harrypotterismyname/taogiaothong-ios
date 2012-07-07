706ea885a8aabadef2fbb8fb1cf91600319274d2
Le Viet Hong <viethong.le@gmail.com>
2012-07-07 11:56:39 +0700

    Initial Commit

---
+++ HappyCarb/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HappyCarb
+//
+//  Created by Hong on 7/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HappyCarbTests/HappyCarbTests.h
@@ -0,0 +1,13 @@
+//
+//  HappyCarbTests.h
+//  HappyCarbTests
+//
+//  Created by Hong on 7/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HappyCarbTests : SenTestCase
+
+@end
