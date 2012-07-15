c75040a2176482dff58ea2eec8360605c40e2993
Le Viet Hong <viethong.le@gmail.com>
2012-07-16 03:12:30 +0700

    textview with placeholder

---
+++ HappyCarb/UIPlaceHolderTextView.m
@@ -0,0 +1,104 @@
+//
+//  UIPlaceHolderTextView.m
+//  HappyCarb
+//
+//  Created by Hong on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//ref: http://stackoverflow.com/questions/1328638/placeholder-in-uitextview
+
+#import "UIPlaceHolderTextView.h"
+
+@implementation UIPlaceHolderTextView
+
+
+@synthesize placeHolderLabel;
+@synthesize placeholder;
+@synthesize placeholderColor;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self setPlaceholder:@""];
+        [self setPlaceholderColor:[UIColor lightGrayColor]];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
+        
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self setPlaceholder:@""];
+    [self setPlaceholderColor:[UIColor lightGrayColor]];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
+}
+
+
+- (void)textChanged:(NSNotification *)notification
+{
+    if([[self placeholder] length] == 0)
+    {
+        return;
+    }
+    
+    if([[self text] length] == 0)
+    {
+        [[self viewWithTag:999] setAlpha:1];
+    }
+    else
+    {
+        [[self viewWithTag:999] setAlpha:0];
+    }
+}
+
+- (void)setText:(NSString *)text {
+    [super setText:text];
+    [self textChanged:nil];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    if( [[self placeholder] length] > 0 )
+    {
+        if ( placeHolderLabel == nil )
+        {
+            placeHolderLabel = [[UILabel alloc] initWithFrame:CGRectMake(8,8,self.bounds.size.width - 16,0)];
+            placeHolderLabel.lineBreakMode = UILineBreakModeWordWrap;
+            placeHolderLabel.numberOfLines = 0;
+            placeHolderLabel.font = self.font;
+            placeHolderLabel.backgroundColor = [UIColor clearColor];
+            placeHolderLabel.textColor = self.placeholderColor;
+            placeHolderLabel.alpha = 0;
+            placeHolderLabel.tag = 999;
+            [self addSubview:placeHolderLabel];
+        }
+        
+        placeHolderLabel.text = self.placeholder;
+        [placeHolderLabel sizeToFit];
+        [self sendSubviewToBack:placeHolderLabel];
+    }
+    
+    if( [[self text] length] == 0 && [[self placeholder] length] > 0 )
+    {
+        [[self viewWithTag:999] setAlpha:1];
+    }
+    
+    [super drawRect:rect];
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
